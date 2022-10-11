0cd7bae501c3f7819cd44c4f39512f246941d24f
kishore951 <kishoresakthivel951@gmail.com>
2022-10-11 22:05:07 +0530

    over

---
+++ src/app/api.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor( private http:HttpClient) { }
+  userData=()=>{
+    return this.http.get("https://reqres.in/api/users?page=1")
+  }
+}
